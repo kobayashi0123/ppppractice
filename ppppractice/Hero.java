9d82f34636822339202aad743291473b829fb981
kobayashi0123 <xiaolinzhongzhi95@gmail.com>
2021-09-09 09:53:07 +0900

    Create Hero.java

---
+++ ppppractice/Hero.java
@@ -0,0 +1,14 @@
+public class Hero{
+	String name;
+	String job;
+	int hp;
+
+	public Hero(String name,String job,int hp){
+		this.name = name;
+		this.job = job;
+		int hp = hp;
+	}
+	public Hero(){
+		this("勇者A","勇者",100);
+	}
+
---
+++ ppppractice/Main.java
@@ -0,0 +1,4 @@
+public class Main{
+	public static void main(String[]ager){
+		Hero h = new Hero();
+		System.out.println(h.name);
